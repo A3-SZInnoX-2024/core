4f2d69f6473106ad1ac11b0bb97461a799947261
Wu Chengyu <54303040+7086cmd@users.noreply.github.com>
2024-02-04 16:40:08 +0800

    basic: block & location

--- src/main.py
+++ src/main.py
@@ -7,75 +7,23 @@ sio = AsyncServer(cors_allowed_origins="*")
 socket = web.Application()
 sio.attach(socket)
 
-arm_status = "stopped"
-status = "stopped"
-stamp_arm = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-location = {"x": 0, "y": 0, "z": 0, "roll": 0, "pitch": 0, "yaw": 0}
-stamp_move = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+@sio.on('connect')
+async def connect(sid, environ):
+    print(f"connected {sid}")
 
+@sio.on('disconnect')
+async def disconnect(sid):
+    print(f"disconnected {sid}")
 
-@sio.on("location")
-async def load_location(sid, data: np.ndarray):
-    print(data)
-    global location, stamp_move
-    location = {
-        "x": data[0],
-        "y": data[1],
-        "z": data[2],
-        "roll": data[3],
-        "pitch": data[4],
-        "yaw": data[5],
-    }
-    stamp_move = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+@sio.on('location-cv')
+async def location_cv(sid, x, y, theta):
+    print(f"Received location data from cv: {x}, {y}, {theta}")
+    await sio.emit('location-rc', (x, y, theta))
 
-@sio.on('move_to')
-async def move_to(sid, x: float, y: float, theta: float):
-    global status
-    status = "moving"
-    print(f'Moving to x={x} y={y} theta={theta}')
-    await sio.emit('robot_motion_start', data=(x, y, theta))
-
-@sio.on("robot_motion_success")
-async def robot_motion(sid, motion_status: str):
-    global status, stamp_move
-    status = "stopped"
-    stamp_move = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    await sio.emit("robot_motion_end", data={"status": motion_status})
-
-
-@sio.on("robot_motion_failure")
-async def robot_motion(sid, motion_status: str):
-    global status, stamp_move
-    status = "stopped"
-    stamp_move = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    await sio.emit("robot_motion_end", data={"status": motion_status})
-
-
-@sio.on("arm_success")
-async def arm_motion(sid, status: str):
-    global arm_status, stamp_arm
-    arm_status = status
-    stamp_arm = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    await sio.emit("arm_motion_end", data={"status": status})
-
-
-@sio.on("arm_failure")
-async def arm_motion(sid, status: str):
-    global arm_status, stamp_arm
-    arm_status = "stopped"
-    stamp_arm = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    await sio.emit("arm_motion_end", data={"status": status})
-
-
-@sio.on("arm_motion_start")
-async def arm_motion(sid, data: str):
-    with open("log.txt", "a") as f:
-        f.write(f"{data}\n")
-    global arm_status
-    arm_status = "moving"
-    print(data, arm_status)
-    await sio.emit("arm_motion_start", data={"status": "moving"})
-
+@sio.on('block-cv')
+async def block_cv(sid, x, y, color: str):
+    print(f"Received block data from cv: {x}, {y}, {color}")
+    await sio.emit('block-rc', (x, y, color))
 
 if __name__ == "__main__":
     web.run_app(socket, port=8000)
